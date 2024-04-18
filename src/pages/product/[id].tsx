ebda5ab48b2fddf9ccce9ea41d3172885fea5d92
gilvania moreira <moreiragil84@gmail.com>
2024-04-18 15:27:08 -0300

    feat: routes api

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -39,11 +39,9 @@ export default function Product({ product }: ProductProps) {
 export const getStaticPaths = async () => {
   return (
     paths: [
-      {
-        params: { id: 'prod_MLH5Wy0Y97hDAC' }
-      }
+      { params: { id: 'prod_MLH5Wy0Y97hDAC' }}
     ],
-    fallback: false,
+    fallback: 'bloking',
   )
 }
 
