54dcb9cea29265fbcc58d02d84121041a3c2d219
gilvania moreira <moreiragil84@gmail.com>
2024-04-18 21:49:24 -0300

    feat: seo working

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps } from "next";
 import Image from "next/image";
+import Head from "next/head";
 import Link from "next/link";
 import Stripe from "stripe";
 import { stripe } from "../lib/stripe";
@@ -16,21 +17,29 @@ interface SuccessProps {
 export default function Success({ costumerName, product
  }: SuccessProps) {
   return (
-    <SuccessContainer>
-      <h1>Compra efetuada</h1>
+    <>
+      <Head>
+        <title>Compra efetuada | Ignite Shop</title>
+        <meta name="robots" content="noindex" />
+      </Head>
 
-      <ImageContainer>
-        <Image src={product.imageUrl} width={120} height={110} alt="" />
-      </ImageContainer>
+      <SuccessContainer>
+        <h1>Compra efetuada</h1>
 
-      <p>
-        Uhuul <strong>{costumerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
-      </p>
 
-      <Link href="/">
-        Voltar ao catálogo
-      </Link>
-    </SuccessContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={120} height={110} alt="" />
+        </ImageContainer>
+
+        <p>
+          Uhuul <strong>{costumerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
+        </p>
+
+        <Link href="/">
+          Voltar ao catálogo
+        </Link>
+      </SuccessContainer>
+    </>
   )
 }
 
@@ -43,7 +52,7 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
       }
     }
   }
-  
+
   const sessionId = String(query.session_id);
 
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
